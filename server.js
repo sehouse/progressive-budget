97d00042c5b4c4dd1da4fb269cc6b8c243381fc1
sehouse <sehouse@umich.edu>
2020-07-31 19:19:43 -0400

    Connecting app to heroku.

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI);
 
 const app = express();
 
